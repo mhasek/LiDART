390b8f187df936cff81465e008314d077919753e
sherrychen1120 <sherrychen961120@gmail.com>
2018-10-21 18:06:00 -0400

    integration V2

--- lidart_scan_matching/scripts/scan_matcher.py
+++ lidart_scan_matching/scripts/scan_matcher.py
@@ -12,7 +12,7 @@ from geometry_msgs.msg import Point
 pub = rospy.Publisher('scan_match_location', Point, queue_size=10)
 
 # macros
-N = 1081
+N = 720
 
 # Callback for receiving LIDAR data on the /scan topic.
 # data: the LIDAR data, published as a list of distances to the wall.
@@ -23,6 +23,7 @@ curr_x = 0 #???is the initial location 0,0?? TODO
 curr_y = 0 #???is the initial location 0,0?? TODO 
 curr_direction =  0 # initialized to be 0 (pointing in +x) degrees. positive is counter-clockwise
 def scan_callback(data):
+	global is_first_time
 	global prev_scan
 	global curr_scan
 	global curr_direction
@@ -30,6 +31,7 @@ def scan_callback(data):
 	curr_scan = process_scan(data)
 
 	if not is_first_time:
+
 		# given 2 lidar scans, get the q that minimizes the error function
 		q = iterative_find_q(curr_scan, prev_scan)
 		# use the q to calculate current location
@@ -57,11 +59,11 @@ def scan_callback(data):
 
 # --------------------- HELPER FUNCTIONS RELATED TO LIDAR SCAN PROCESSING ------------------------ #
 # values used for laser scan processing
-START_ANGLE = -135
-END_ANGLE = 135
-ANGLE_SPAN = END_ANGLE - START_ANGLE
-THETA_DELTA = (END_ANGLE - START_ANGLE)/N
-thetas = np.arange(START_ANGLE,END_ANGLE,THETA_DELTA)
+start_angle = -135
+end_angle = 135
+angle_span = end_angle - start_angle
+theta_delta = (end_angle - start_angle) * 1.0 /N
+thetas = np.arange(start_angle,end_angle,theta_delta)
 # ACCEPTABLE_DISTANCE = sys.float_info.max
 
 # Process laser scan and save as cartesian
@@ -75,7 +77,7 @@ def process_scan(data):
 	# converted to cartesian
 	scan_x = scans*np.cos(thetas)
 	scan_y = scans*np.sin(thetas)
-	data_xy = np.hstack((scan_x,scan_y))
+	data_xy = np.hstack((scan_x.reshape(-1,1),scan_y.reshape(-1,1)))
 
 	return data_xy
 
@@ -93,24 +95,24 @@ def iterative_find_q(curr_scan, prev_scan):
 	error = N
 
 	# while error(q_(k+1), C_k) is larger than a threshold:
-	while (error > threshold){
+	while (error > threshold):
 		# C_(k+1) = search_correspondence(curr_scan, prev_scan, q_(k+1))
 		C = search_correspondence(curr_scan, prev_scan, q)
 		# q_(k+2) = get_q(curr_scan, prev_scan, C_(k+2)) #TODO
 		q = get_q(curr_scan, prev_scan, C)
 		# calculate error(q_(k+2), C_(k+1))
 		error = calculate_error(curr_scan, prev_scan, C, q)
-	}
 
 	# return q
 	return q
 
 def calculate_error(curr_scan, prev_scan, C, q):
-	project_p2p = np.matmul(rot(q[2]),curr_scan.T).T + np.tile(q[:2], (5,1)) - prev_scan[C,:]
-	segments = prev_scan[C,:] - prev_scan[[(C[i] - 1) if (C[i]+1 >= len(C))  else (C[i] + 1) for i in range(5)],:]
+	project_p2p = np.matmul(rot(q[2]),curr_scan.T).T + np.tile(q[:2], (N,1)) - prev_scan[C[:,1],:]
+	segments = prev_scan[C[:,1],:] - prev_scan[C[:,2],:]
 	normals = np.array([-segments[:,1], -segments[:,0]]).T
 	normals_lengths = np.sqrt(np.sum(normals**2, axis = 1))
-	normals = normals / normals_lengths.reshape(5,1)
+	normals_lengths
+	normals = normals / normals_lengths.reshape(N,1)
 	project_p2l = np.sum(np.sum(np.multiply(normals,project_p2p), axis=1)**2)
 	return project_p2l
 
@@ -118,18 +120,20 @@ def calculate_error(curr_scan, prev_scan, C, q):
 # Search the closest correspondence between curr_scan and prev_scan
 # input: curr_scan, prev_scan: both lidar scans; 
 # q: roto-transformation guess from curr_scan to prev_scan [t_x, t_y, theta]
-# output: C[N,1]: i-th scan in curr_scan corresponds to C[i]-th scan in prev_scan
+# output: C[N,3]: [i,ji1,ji2] - i-th scan in current scan corresponds to ji1 and ji2 in previous scan
 def search_correspondence(curr_scan, prev_scan, q):
 	# old scan: prev_scan
 	old_x = prev_scan[:,0]
 	old_y = prev_scan[:,1]
 	# world scan: curr_scan projected onto prev_scan after transform q
-	world_x = np.matmul(rot(q[2]), curr_scan[:,0]) + q[0]
-	world_y = np.matmul(rot(q[2]), curr_scan[:,1]) + q[1]
+	world_xy = np.matmul(rot(q[2]), curr_scan.T) + np.tile((q[:2].reshape(-1,1)),(1,N))
+	world_x = world_xy[0,:]
+	world_y = world_xy[1,:]
 
 	last_best = -1
 
-	pairings = np.empty([N, 1])
+	C = np.zeros([N, 3]).astype(int)
+	C[:,0] = np.arange(N)
 
 	# ??? What does this do
 	up_out, up_in = getUpArrays(old_x, old_y, N)
@@ -144,7 +148,7 @@ def search_correspondence(curr_scan, prev_scan, q):
 	  
 	  	start_index = last_best
 	  	if (start_index == -1):
-	    	start_index = i
+	  		start_index = i
 	  
 	  	up = start_index + 1
 	  	down = start_index
@@ -158,66 +162,68 @@ def search_correspondence(curr_scan, prev_scan, q):
 		# up stopped becomes true when no angle can become closer
 		# down stopped becoems true when no smaller angle can become true
 		while not up_stopped or not down_stopped:
-		    up = int(up)
-		    down = int(down)
+			up = int(up)
+			down = int(down)
 
-		    now_up = not up_stopped and (last_dist_up <= last_dist_down)
-		    if (now_up):
-		      	if (up >= N):
-		        	up_stopped = True
-		        	continue
+			now_up = not up_stopped and (last_dist_up <= last_dist_down)
+			if (now_up):
+				if (up >= N):
+					up_stopped = True
+					continue
 		        
-		      	last_dist_up = (old_x[up] - world_x[i])**2 + (old_y[up] - world_y[i])**2
-		      	if (last_dist_up < best_dist and last_dist_up < ACCEPTABLE_DISTANCE):
-		        	best = up
-		        	best_dist = last_dist_up
+				last_dist_up = (old_x[up] - world_x[i])**2 + (old_y[up] - world_y[i])**2
+				if (last_dist_up < best_dist and last_dist_up < ACCEPTABLE_DISTANCE):
+					best = up
+					best_dist = last_dist_up
 		        
-		      	if (isAngleBigger(world_x[i], world_y[i], old_x[up], old_y[up])):
-		        	angle_offset = (up - i) / (N - 1.0) * ANGLE_SPAN + START_ANGLE
-		        	min_dist_up = np.sin(np.deg2rad(angle_offset)) * world_norm
-		        	if (min_dist_up > best_dist):
-		        	  	up_stopped = True
-		          		continue
-		           
-			        if (old_x[up])**2 + old_y[up]**2 > world_norm:
-			          	up = up_in[up][0]
-			        else:
-			          	up = up_out[up][0]
-		      	else:
-		        	up += 1
-		      	
-		      	if (up < 0 or up >= N):
-		          	up_stopped = True
+				if (isAngleBigger(world_x[i], world_y[i], old_x[up], old_y[up])):
+					angle_offset = (up - i) / (N - 1.0) * ANGLE_SPAN + START_ANGLE
+					min_dist_up = np.sin(np.deg2rad(angle_offset)) * world_norm
+					if (min_dist_up > best_dist):
+						up_stopped = True
+						continue
+					
+					if (old_x[up])**2 + old_y[up]**2 > world_norm:
+						up = up_in[up][0]
+					else:
+						up = up_out[up][0]
+				else:
+					up += 1
+				
+				if (up < 0 or up >= N):
+					up_stopped = True
 		            
-		    if (not now_up):
-		      	if (down < 0):
-		        	down_stopped = True
-		        	continue
+			if (not now_up):
+				if (down < 0):
+					down_stopped = True
+					continue
+				
+				last_dist_down = (old_x[down] - world_x[i])**2 + (old_y[down]- world_y[i])**2
+				if (last_dist_down < best_dist and last_dist_down < ACCEPTABLE_DISTANCE):
+					best = down
+					best_dist = last_dist_down
 		        
-		      	last_dist_down = (old_x[down] - world_x[i])**2 + (old_y[down]- world_y[i])**2
-		      	if (last_dist_down < best_dist and last_dist_down < ACCEPTABLE_DISTANCE):
-		        	best = down
-		        	best_dist = last_dist_down
-		        
-		      	if (isAngleBigger(old_x[down], old_y[down],world_x[i], world_y[i])):
-		        	angle_offset = (i - down) / (N - 1.0) * ANGLE_SPAN + START_ANGLE
-		        	min_dist_down = np.sin(np.deg2rad(angle_offset)) * world_norm
-		        	if (min_dist_down > best_dist):
-		          		down_stopped = True
-		          		continue
+				if (isAngleBigger(old_x[down], old_y[down],world_x[i], world_y[i])):
+					angle_offset = (i - down) / (N - 1.0) * ANGLE_SPAN + START_ANGLE
+					min_dist_down = np.sin(np.deg2rad(angle_offset)) * world_norm
+					if (min_dist_down > best_dist):
+						down_stopped = True
+						continue
 		            
-		        if (old_x[down]**2 + old_y[down]**2 > world_norm):
-		          	down = down_in[down][0]
-		        else:
-		          	down = down_out[down][0]
-		      else:
-		        down -= 1
-		      if (down < 0 or down >= N): down_stopped = True
+					if (old_x[down]**2 + old_y[down]**2 > world_norm):
+						down = down_in[down][0]
+					else:
+						down = down_out[down][0]
+				else:
+					down -= 1
+			
+				if (down < 0 or down >= N): down_stopped = True
 		          
-		pairings[i] = best
+		C[i,1] = best
+		C[i,2] = (C[i,1] - 1) if (C[i,1]+1 >= N)  else (C[i,1] + 1)
 		last_best = best
 
-	return pairings
+	return C
 
 
 
@@ -283,6 +289,7 @@ def getUpArrays(x, y, N):
   for i in range(N-1, -1, -1):
     
     r_array = x[i]**2 + y[i]**2
+    pdb.set_trace() #BUG!!!
     r = r_array[0]
     
     lastOutIndex = -1
@@ -341,7 +348,76 @@ def isAngleBigger(x1, y1, x2, y2):
 
 
 # --------------------- HELPER FUNCTIONS RELATED TO GET Q ------------------------ #
+def get_q(p1,p2,C):
 
+
+	M = np.zeros((4,4))
+	g = np.zeros((4,1))
+	x = np.zeros((4,1))
+	W = np.zeros((4,4))
+
+	for i in range(len(C)):
+
+		M_k = np.array([[1 , 0, p1[C[i,0],0], -p1[C[i,0],1] ], \
+			[ 0 , 1, p1[C[i,0],1], p1[C[i,0],0] ]])
+
+		n_k = np.matmul(normalize(p2[C[i,1],:] - p2[C[i,2],:]).reshape(1,2),rot(3.14159/2.)).reshape(2,1)
+
+		C_k = np.matmul(n_k,n_k.T)
+
+		M += np.matmul(np.matmul(M_k.T,C_k),M_k)
+
+		Pi_k = p2[C[i,1],:].reshape(2,1)
+
+		g += -2*np.matmul(Pi_k.T , np.matmul(C_k,M_k)).T
+
+	W[[2,3],[2,3]] = 1
+	W = np.matrix(W)
+	A = np.matrix(2*M[0:2,0:2])
+	B = np.matrix(2*M[0:2,2:4])
+	D = np.matrix(2*M[2:4,2:4])
+	g = np.matrix(g)
+	M = np.matrix(M)
+
+	S = D - (B.T*(A.I)*B)
+
+	Sa = S.I * np.linalg.det(S)
+
+	p_lambda = np.array([4., (2.*S[0,0]+2.*S[1,1]), np.linalg.det(S) ]);
+
+	rhs = getRHS(A, B, Sa, g)
+
+	roots = np.roots(rhs - np.convolve(p_lambda,p_lambda))
+
+	lam = np.real(np.max(roots[np.isreal(roots)]))
+
+	fx = lambda l: -((2*M + 2*l*W).I).T*g
+
+	x = fx(lam)
+
+	theta = np.arctan(x[3,0]/x[2,0])
+
+	print "error: ",(g.T*((2*M + 2*lam*W).I)*W*((2*M + 2*lam*W).I).T*g)[0,0] - 1
+
+	return np.array([x[0,0],x[1,0],theta]).T
+
+# normalize vector
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0: 
+       return v
+    return v / norm
+
+#TODO: what does this do???
+def getRHS(A,B,Sa,g):
+
+	g1 = g[0:2]; g2=g[2:4];
+
+   	p7 = np.array([0,0,( g1.T*((A.I)*B*  4    *B.T*(A.I))*g1 + 2*g1.T*(-(A.I)*B*  4   )*g2  + g2.T*( 4   )*g2)[0,0], \
+	      ( g1.T*((A.I)*B*  4*Sa *B.T*(A.I))*g1 + 2*g1.T*(-(A.I)*B*  4*Sa)*g2  + g2.T*( 4*Sa)*g2)[0,0], \
+			( g1.T*((A.I)*B* Sa*Sa *B.T*(A.I))*g1 + 2*g1.T*(-(A.I)*B* Sa*Sa)*g2 + g2.T*(Sa*Sa)*g2)])
+
+   	return p7
 
 
 # Boilerplate code to start this ROS node.
