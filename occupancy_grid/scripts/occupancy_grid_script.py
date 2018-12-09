f30d87cd55e2162f6ead41c36a2be3471baad444
nkrishnan244 <nkrishnan244@gmail.com>
2018-12-09 03:15:40 +0000

    Updated occupancy grid - updated planner

---
+++ occupancy_grid/scripts/occupancy_grid_script.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from std_msgs.msg import String
+from std_msgs.msg import Int16
+from sensor_msgs.msg import LaserScan
+import math
+import pdb
+import matplotlib.pyplot as plt
+from rospy.numpy_msg import numpy_msg
+# from rospy_tutorials.msg import Floats
+from nav_msgs.msg import Odometry
+from occupancy_grid.msg import OccupancyGrid
+from occupancy_grid.srv import *
+from geometry_msgs.msg import Point
+from tempfile import TemporaryFile
+from rospy.numpy_msg import numpy_msg
+
+
+theta_array = []
+first_scan = True
+counter = 0
+
+OCCUPANCY_GRID_HEIGHT = 3.0 # in meters
+OCCUPANCY_GRID_WIDTH = 3.0 # in meters
+RESOLUTION = 8.0 # boxes per meter
+PIXEL_HEIGHT = int(OCCUPANCY_GRID_HEIGHT*RESOLUTION)
+PIXEL_WIDTH = int(OCCUPANCY_GRID_WIDTH*RESOLUTION)
+
+current_occupancy_grid = np.zeros((PIXEL_WIDTH, PIXEL_HEIGHT))
+radius_map = np.zeros((PIXEL_WIDTH, PIXEL_HEIGHT))
+theta_map = np.zeros((PIXEL_WIDTH, PIXEL_HEIGHT))
+
+current_odom = Odometry()
+occupancy_grid_pub = OccupancyGrid()
+next_point_ = Point()
+
+def create_theta_array(scan):
+    theta_min = scan.angle_min
+    theta_max = scan.angle_max
+    theta_delta = (theta_max - theta_min)/len(np.array(scan.ranges))
+    theta = np.arange(theta_min,theta_max,theta_delta)
+    return theta
+
+def create_radius_array():
+    global radius_map
+    global theta_map
+    for row in range(0, PIXEL_WIDTH):
+        for col in range(0, PIXEL_HEIGHT):
+            theta_map[row, col] = np.arctan2(PIXEL_HEIGHT - (row + 1), col - PIXEL_WIDTH/2)
+            radius_map[row, col] = np.sqrt((col - PIXEL_WIDTH/2)**2 + (PIXEL_HEIGHT - (row + 1))**2)
+            # x_local = row - PIXEL_WIDTH/2
+            # radius_map[col, row] = np.sqrt(x_local**2 + col**2)
+            # theta_map[col, row] = np.arctan2(col, x_local)
+
+
+def callback(data):
+    global occupancy_np
+    global counter
+    counter += 1
+    # updates global occupancy grid so we can have constant publishing
+    global current_occupancy_grid
+    global theta_array
+    global first_scan
+    next_point_ = get_next_point_client()
+    occupancy_grid_pub.next_point = next_point_
+    occupancy_grid_pub.current_odometry = current_odom
+
+    # Only get theta array once
+    if first_scan:
+        theta_array = create_theta_array(data)
+        first_scan = False
+
+    scans = np.array(data.ranges) # SCANS
+    angles = theta_array
+
+    # Filter out NaN's and InF's
+    filtered_scan = scans[np.isfinite(scans)]
+    filtered_angles = angles[np.isfinite(scans)]
+
+    # converts scans to x and y
+    x = filtered_scan*np.sin(filtered_angles + np.deg2rad(90))
+    y = filtered_scan*np.cos(filtered_angles + np.deg2rad(90))
+
+    # Puts values in given box size for occupancy grid
+    box_constraint = (x < OCCUPANCY_GRID_HEIGHT) & (x > 0) & (np.abs(y) < OCCUPANCY_GRID_WIDTH/2.0)
+    x = x[box_constraint]
+    y = y[box_constraint]
+
+
+    # Converts to numpy array centered at top left
+    occupancy_from_car = np.zeros((PIXEL_WIDTH, PIXEL_HEIGHT))
+
+    y_np = (PIXEL_HEIGHT - x*RESOLUTION).astype(int)
+    x_np = (y*RESOLUTION + PIXEL_WIDTH/2.0).astype(int)
+    occupancy_from_car[y_np, x_np] = 1
+
+    pairs = np.array([x_np, y_np]).T
+
+    # Efficiently remove duplicates
+    mult = np.random.rand(pairs.shape[1])
+    ans = pairs.dot(mult)
+    unique, index = np.unique(ans, return_index=True)
+    unique_pairs = pairs[index]
+    # pdb.set_trace()
+
+    # Raytraces obstacles
+    current_radius_map = radius_map
+    current_theta_map = theta_map
+    filtered_scan = filtered_scan[box_constraint]
+    filtered_angles = filtered_angles[box_constraint]
+
+    # mark behind scans as black
+    for pair in unique_pairs:
+        x_ = pair[0]
+        y_ = pair[1]
+        x_loc = PIXEL_HEIGHT - (y_ + 1)
+        y_loc = x_ - PIXEL_WIDTH/2.0
+        r_loc = np.sqrt((x_loc)**2 + (y_loc)**2)
+        theta = np.arctan2(x_loc, y_loc)
+        raytrace_mask = (current_radius_map > r_loc) & (np.abs(current_theta_map - theta) < np.deg2rad(5.0))
+        occupancy_from_car[raytrace_mask] = 1.0
+
+    ## THIS WILL SHOW YOU
+    # plt.axis([0, int(OCCUPANCY_GRID_WIDTH*RESOLUTION), 0, int(OCCUPANCY_GRID_HEIGHT*RESOLUTION)])
+    # np.save("Left_Turn", occupancy_from_car)
+    # pdb.set_trace()
+
+    # if counter % 100 == 0:
+    #     print("PLOT")
+    #     plt.gcf().clear()
+    #     plt.ion()
+    #     plt.imshow(occupancy_from_car, cmap='gray_r')
+    #     plt.pause(0.0000001)
+    #     plt.show()
+
+    # Puts coordinates in numpy frame
+    # x_prime = y + OCCUPANCY_GRID_WIDTH/2.0
+    # y_prime = -x + OCCUPANCY_GRID_HEIGHT
+
+    # Convert numpy frame to indices
+    # x_index = (x_prime*RESOLUTION).astype(int)
+    # y_index = (y_prime*RESOLUTION).astype(int)
+
+    # Inserts obstacles into occupancy array
+    # occupancy = np.zeros((int(OCCUPANCY_GRID_WIDTH*RESOLUTION), int(OCCUPANCY_GRID_HEIGHT*RESOLUTION)))
+    # occupancy[y_index, x_index] = 1
+    # plt.imshow(occupancy, cmap='gray_r')
+    # plt.axis([0, int(OCCUPANCY_GRID_WIDTH*RESOLUTION), 0, int(OCCUPANCY_GRID_HEIGHT*RESOLUTION)])
+    # plt.show()
+    # plt.gcf().clear()
+    current_occupancy_grid = occupancy_from_car
+    occupancy_list = current_occupancy_grid.reshape(int(PIXEL_HEIGHT*PIXEL_WIDTH)).tolist()
+    occupancy_grid_pub.grid_path = occupancy_list
+    # occupancy_grid_pub.occupancy_grid = current_occupancy_grid
+
+    # occupancy_np.data = current_occupancy_grid.reshape(int(PIXEL_HEIGHT*PIXEL_WIDTH))
+    # occupancy_grid_pub.occupancy_grid = occupancy_np
+
+def odom_callback(data):
+    global current_odom
+    current_odom = data
+
+def next_point_callback(data):
+    global next_point_
+    next_point_ = data
+
+def get_next_point_client():
+    rospy.wait_for_service('get_last_waypoint_in_neighborhood')
+    get_last_waypoint = rospy.ServiceProxy('get_last_waypoint_in_neighborhood', GetLastBoxPoint)
+    try:
+        resp1 = get_last_waypoint(current_odom)
+        return resp1
+        next_point_ = resp1.last_box_waypoint
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+if __name__ == '__main__':
+    rospy.init_node('generate_occupancy_grid', anonymous=True)
+    rate = rospy.Rate(10)
+    rospy.Subscriber("/scan", LaserScan, callback)
+    rospy.Subscriber("/pf/pose/odom", Odometry, odom_callback)
+    rospy.Subscriber("/next_point", Point, next_point_callback)
+
+    # pub = rospy.Publisher('grid_path', numpy_msg(Floats), queue_size=10)
+    create_radius_array()
+
+    # Comment out
+    # pub = rospy.Publisher('grid', OccupancyGrid, queue_size=10)
+    pub = rospy.Publisher('/grid', OccupancyGrid, queue_size=10)
+
+    # pub = rospy.Publisher('floats', numpy_msg(Floats), queue_size=10)
+    while not rospy.is_shutdown():
+        pub.publish(occupancy_grid_pub)
+        # print("PUBLISHING")
+        rate.sleep()
+        # pub.publish(current_occupancy_grid)
+        # rate.sleep()
---
+++ occupancy_grid/scripts/planner.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+import numpy as np
+import math
+from geometry_msgs.msg import Point
+import csv
+import os
+import sys
+import pdb
+import rospy
+from occupancy_grid.srv import *
+from occupancy_grid.msg import LastBoxWaypoint
+from tf.transformations import euler_from_quaternion
+from tf.transformations import quaternion_from_euler
+
+class Planner(object):
+  def __init__(self, global_waypoints):
+    # global_waypoints are a numpy array of waypoints
+    rospy.init_node('planner_node')
+    s1 = rospy.Service('get_last_waypoint_in_neighborhood', GetLastBoxPoint, self.getLastWaypointInNeighborhoodService)
+    s2 = rospy.Service('get_next_pursuit_point', GetNextPursuitPoint, self.getNextWaypointService)
+    self.global_waypoints = global_waypoints
+    self.next_lap_plus = np.append(global_waypoints, global_waypoints, axis=0)
+    self.next_lap_starts_at = len(global_waypoints)
+    rospy.spin()
+
+  ## Service, returns next point
+  def getNextWaypoint(self, current_location, radius):
+    next_waypoint = Point()
+    for i in range(len(self.next_lap_plus)):
+      if (np.linalg.norm(current_location - self.next_lap_plus[i, :]) >= radius):
+        if (i == 0):
+          raise Exception("First waypoint is too far away from current location")
+
+        # Remove waypoints that have been passed
+        self.next_lap_plus = self.next_lap_plus[i-1:,:]
+
+        # Update the pointer to the start of the next lap
+        self.next_lap_start_at = self.next_lap_starts_at - i + 1
+
+        # If less than a lap remains, add another lap of global waypoints
+        if (self.next_lap_starts_at <= 0):
+          self.next_lap_starts_at = len(self.next_lap_plus)
+          self.next_lap_plus = np.append(self.next_lap_plus, global_waypoints, axis=0)
+
+        next_waypoint.x = self.next_lap_plus[0, 0]
+        next_waypoint.y = self.next_lap_plus[0, 1]
+        return next_waypoint
+        # return self.next_lap_plus[0,:]
+    raise Exception("All waypoints are too close to current location")
+
+  def getLastWaypointInNeighborhood(self, x, y, theta, neighborhood_length):
+    last_point = Point()
+    last_box_waypoint = LastBoxWaypoint()
+    for i in range(len(self.next_lap_plus)):
+      dx = self.next_lap_plus[i, 0] - x
+      dy = self.next_lap_plus[i, 1] - y
+      if (abs(dx * math.cos(theta) + dy * math.sin(theta)) > neighborhood_length):
+        # Exit Forward
+        # print("Exit Forward")
+        last_box_waypoint.out_direction = 0
+        last_point.x = self.next_lap_plus[i - 1, 0]
+        last_point.y = self.next_lap_plus[i - 1, 1]
+        last_box_waypoint.last_waypoint_in_neighborhood = last_point
+        return last_box_waypoint
+      if (dx * math.sin(theta) - dy * math.cos(theta) > neighborhood_length / 2):
+        # Exit Right
+        # print("Exit Right")
+        last_box_waypoint.out_direction = 1
+        last_point.x = self.next_lap_plus[i - 1, 0]
+        last_point.y = self.next_lap_plus[i - 1, 1]
+        last_box_waypoint.last_waypoint_in_neighborhood = last_point
+        return last_box_waypoint
+      if (-dx * math.sin(theta) + dy * math.cos(theta) > neighborhood_length / 2):
+        # Exit Left
+        # print("Exit Left")
+        last_box_waypoint.out_direction = -1
+        last_point.x = self.next_lap_plus[i - 1, 0]
+        last_point.y = self.next_lap_plus[i - 1, 1]
+        last_box_waypoint.last_waypoint_in_neighborhood = last_point
+        return last_box_waypoint
+    raise Exception("All waypoints are in the neighborhood")
+
+  def updateWaypoints(self, new_waypoints, last_waypoint):
+    for i in range(len(self.next_lap_plus)):
+      if (np.array_equal(last_waypoint, self.next_lap_plus[i, :])):
+        print(new_waypoints)
+        print(self.next_lap_plus)
+        self.next_lap_plus = np.append(new_waypoints, self.next_lap_plus[i + 1:, :], axis=0)
+
+        # If less than a lap remains, add another lap of global waypoints
+        if (self.next_lap_starts_at <= i):
+          self.next_lap_starts_at = len(self.next_lap_plus)
+          self.next_lap_plus = np.append(self.next_lap_plus, global_waypoints, axis=0)
+
+        return
+    raise Exception("That waypoint does not exist")
+
+  def getLastWaypointInNeighborhoodService(self, req):
+      neighborhood_len = 3.0
+      x = req.current_odom.pose.pose.position.x
+      y = req.current_odom.pose.pose.position.y
+      euler = euler_from_quaternion((req.current_odom.pose.pose.orientation.x, req.current_odom.pose.pose.orientation.y,
+                                     req.current_odom.pose.pose.orientation.z, req.current_odom.pose.pose.orientation.w)) # Make a new service
+      theta = euler[2]
+      last_waypoint_in_neighborhood = self.getLastWaypointInNeighborhood(x, y, theta, neighborhood_len)
+      # MAKE A MSG TYPE FOR THIS
+      return last_waypoint_in_neighborhood
+
+  def getNextWaypointService(self, req):
+      position = np.array([req.current_odom.pose.pose.position.x, req.current_odom.pose.pose.position.y])
+      radius = req.radius
+      next_way = self.getNextWaypoint(position, radius)
+      return next_way
+
+def read_csv():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../waypoints/test.csv')
+    with open(filename) as f:
+        path_points = [tuple(line) for line in csv.reader(f)]
+
+    # Turn path_points into a list of floats to eliminate the need for casts in the code below.
+    path_points = [[float(point[0]), float(point[1]), float(point[2])] for point in path_points]
+    # change path_points into np array to simply processing
+    path_points = np.array(path_points)[:,:2]
+    return path_points
+
+if __name__ == '__main__':
+    path_points = read_csv()
+    planner = Planner(path_points)
